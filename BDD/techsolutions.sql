cf0ab53aa83d729d294df12c9e7f3d7b0f2a9824
COUBERES Alexandre <alexandrecouberes@gmail.com>
2025-11-26 09:37:00 +0100

    Sauvegarde

---
+++ BDD/techsolutions.sql
@@ -0,0 +1,105 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: techsolutions
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `components`
+--
+
+DROP TABLE IF EXISTS `components`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `components` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `components`
+--
+
+LOCK TABLES `components` WRITE;
+/*!40000 ALTER TABLE `components` DISABLE KEYS */;
+INSERT INTO `components` VALUES (1,'Processeur i5','CPU 6 cœurs polyvalent'),(2,'RAM 16 Go','DDR4 2×8 Go'),(3,'SSD 500 Go','NVMe rapide'),(4,'Carte graphique GTX 1660','Jeux Full HD'),(5,'Boîtier + Alim 500W','Tour + alimentation'),(6,'Intel Core i5-12600K','Processeur MV-SC-RH'),(8,'AMD Ryzen 9 7900 Wraith Prism','Processeur DL'),(10,'Intel Core i9-14900KF','Processeur DUX/UI'),(11,'ASUS ROG STRIX B760-F GAMING WIFI','Carte mère DUX/UI'),(12,'ASRock A620AM-X WiFi','Carte mère DL'),(13,'Fox Spirit EG1 Core\n\n','B1'),(14,'be quiet! Pure Base 600 Window Noir\n','B2');
+/*!40000 ALTER TABLE `components` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pc_components`
+--
+
+DROP TABLE IF EXISTS `pc_components`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pc_components` (
+  `pc_id` int(10) unsigned NOT NULL,
+  `component_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`pc_id`,`component_id`),
+  KEY `component_id` (`component_id`),
+  CONSTRAINT `pc_components_ibfk_1` FOREIGN KEY (`pc_id`) REFERENCES `pcs` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `pc_components_ibfk_2` FOREIGN KEY (`component_id`) REFERENCES `components` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pc_components`
+--
+
+LOCK TABLES `pc_components` WRITE;
+/*!40000 ALTER TABLE `pc_components` DISABLE KEYS */;
+INSERT INTO `pc_components` VALUES (1,2),(1,3),(1,5),(1,8),(1,12),(2,1),(2,2),(2,3),(2,4),(2,5),(3,1),(3,2),(3,3),(3,4),(3,5);
+/*!40000 ALTER TABLE `pc_components` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pcs`
+--
+
+DROP TABLE IF EXISTS `pcs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pcs` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pcs`
+--
+
+LOCK TABLES `pcs` WRITE;
+/*!40000 ALTER TABLE `pcs` DISABLE KEYS */;
+INSERT INTO `pcs` VALUES (1,'PC Développement','https://media.ldlc.com/r705/ld/products/00/06/03/62/LD0006036203_0006056588_0006163342_0006272355.jpg',549.00),(2,'PC Infrastructure','https://www.hp.com/fr-fr/shop/Html/Merch/Images/B6YY6ET-ABF_3_1750x1285.jpg',779.00),(3,'PC Design','https://media.ldlc.com/r705/ld/products/00/06/28/36/LD0006283656.jpg',999.00),(4,'PC ','',500.00),(5,'PC Support Client','https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/desktops/dell/ecs1250/media-gallery/with-reader/desktop-ecs1250-slim-tower-black-gallery-1.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=242&qlt=100,1&resMode=sha',500.00);
+/*!40000 ALTER TABLE `pcs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-26  9:36:59
